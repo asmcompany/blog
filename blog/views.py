bdf9831444ada9a1a42ed8874115375434a531ed
Mhmdbayat7 <mhmddm1998@gmail.com>
2022-09-14 02:08:48 -0700

    mhmd2

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,64 @@
 from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+all_posts = [
+    {
+        'slug': 'learning-django',
+        'title': 'django course',
+        'author': 'Mohammad Ordookhani',
+        'image': 'django.png',
+        'date': date(2021, 4, 5),
+        'short_description': 'this is django course in toplearn from zero to hero',
+        'content': """
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dicta, eius eos eum eveniet
+            perspiciatis quod soluta veritatis? Alias atque ducimus facere odit pariatur! Alias, aspernatur consequuntur
+            deleniti est fugit officiis porro quia saepe tenetur, veniam veritatis voluptatem voluptatum?
+        """
+    },
+    {
+        'slug': 'learning-python',
+        'title': 'python course',
+        'author': 'Mohammad Ordookhani',
+        'image': 'python.png',
+        'date': date(2021, 6, 3),
+        'short_description': 'this is django course in toplearn from zero to hero',
+        'content': """
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dicta, eius eos eum eveniet
+            perspiciatis quod soluta veritatis? Alias atque ducimus facere odit pariatur! Alias, aspernatur consequuntur
+            deleniti est fugit officiis porro quia saepe tenetur, veniam veritatis voluptatem voluptatum?
+        """
+    },
+    {
+        'slug': 'learning-machine-learning',
+        'title': 'ml course',
+        'author': 'Mohammad Ordookhani',
+        'image': 'ml.png',
+        'date': date(2021, 3, 1),
+        'short_description': 'this is django course in toplearn from zero to hero',
+        'content': """
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dicta, eius eos eum eveniet
+            perspiciatis quod soluta veritatis? Alias atque ducimus facere odit pariatur! Alias, aspernatur consequuntur
+            deleniti est fugit officiis porro quia saepe tenetur, veniam veritatis voluptatem voluptatum?
+        """
+    },
+]
+
+
+def get_date(post):
+    return post['date']
+
+
 def index(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-2:]
+    return render(request, 'blog/index.html', {
+        'latest_posts': latest_posts
+    })
+
 
 def posts(request):
-    pass
+    return render(request, 'blog/all-posts.html')
 
-def single_post(request):
-    pass
+
+def single_post(request, slug):
+    return render(request, 'blog/post-detail.html')
